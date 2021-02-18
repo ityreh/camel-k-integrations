b03d9b97005c57afe6716dcfef3a7f6a713bf830
Yannick Rehberger <yr@ityreh.de>
2021-02-18 11:26:46 +0100

    Add first google pubsub sample.

---
+++ Google/google-pubsub/PublishHelloWorld.java
@@ -0,0 +1,13 @@
+// camel-k: language=java
+// camel-k: property=projectId=development-281118 property=destinationName=hello-topic
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class PublishHelloWorld extends RouteBuilder {
+  @Override
+  public void configure() throws Exception {
+    from("timer:hello?period=10000").routeId("hello").setBody().simple("Hello World")
+        .to("google-pubsub://{{projectId}}:{{destinationName}}");
+
+  }
+}
